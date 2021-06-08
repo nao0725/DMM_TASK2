75751045fd1921928a045505f4884ac59d9b55a3
nao0725 <aqua.725.times@gmail.com>
2021-06-08 07:47:20 +0000

    created book page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,4 +11,15 @@ class BooksController < ApplicationController
   def edit
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+  
 end
