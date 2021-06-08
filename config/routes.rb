6452c9a9a07cc377584588bef0b0df0fcc22dd49
nao0725 <aqua.725.times@gmail.com>
2021-06-08 03:18:42 +0000

    created routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'book/index'
-  get 'book/show'
-  get 'book/new'
-  get 'book/edit'
+  resources :bookers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "home#index" #ルートパス設定
 end
