0502ec7b738d6a006ba9accdfa8d7c0656ad394e
nao0725 <aqua.725.times@gmail.com>
2021-06-08 02:34:20 +0000

    create controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'book/index'
+  get 'book/show'
+  get 'book/new'
+  get 'book/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
