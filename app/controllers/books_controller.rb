0eb795582f04c9b767e82991621a105d12e66682
nao0725 <aqua.725.times@gmail.com>
2021-06-09 09:03:34 +0000

    Updated show action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,8 +16,8 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(book)
       flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book)
     else
       render:edit
     end
