8ff94f6789373677f4853cab4e1e0013055b0f2e
nao0725 <aqua.725.times@gmail.com>
2021-06-09 08:17:07 +0000

    Updated validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,7 +12,7 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -26,10 +26,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(@book)
       flash[:notice] = "Book was successfully created"
+      redirect_to book_path(@book)
     else
-      render:show
+      @books = Book.all
+      render:index
     end
   end
 
@@ -43,5 +44,5 @@ class BooksController < ApplicationController
   private
     def book_params
       params.require(:book).permit(:title, :body)
-    end 
+    end
 end
