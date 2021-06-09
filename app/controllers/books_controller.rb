b27429159a0971c39824762fde9e41536a725022
nao0725 <aqua.725.times@gmail.com>
2021-06-09 01:03:09 +0000

    add to update action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,12 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
 
   def create
     @book = Book.new(book_params)
