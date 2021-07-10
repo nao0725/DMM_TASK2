98c20a1d349ceda14040e3bb3b8ab514fb937c9e
nao0725 <aqua.725.times@gmail.com>
2021-07-10 01:36:57 +0000

    Add to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  Book.create!(
+    title: 'railsを学ぼう',
+    body: 'seedファイルについて'
+  )
+  
+  10.times do |number|
+    Book.create!(title: 'timesを学習しよう',body: number)
+  end
